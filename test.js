b78466827714c42c4dc803ebf06c45003b332a4d
Josh Matthew Talplacido <joshmatthewtalplacido@gmail.com>
2019-05-30 19:49:35 -0700

    Publishing

--- test.js
+++ test.js
@@ -3,18 +3,18 @@ const genID = require("./genid.js");
 let data = [
   {
     id: genID(),
-    name: "Josh",
-    level: "G12"
+    name: "Marilie",
+    level: "Lead Accounts Assistant"
   },
   {
     id: genID(),
     name: "Clementina",
-    level: "G12"
+    level: "Human Program Technician"
   },
   {
     id: genID(),
     name: "Karli",
-    level: "G12"
+    level: "Customer Identity Engineer"
   }
 ];
 
